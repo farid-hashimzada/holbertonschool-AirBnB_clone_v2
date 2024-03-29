2a09af038643ccb1a761e48c24571d81f4eb79a8
Farid Hashimzade <124304902+farid-hashimzada@users.noreply.github.com>
2024-03-29 18:14:29 +0400

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,29 +1,16 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from os import getenv
 
 
-class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', cascade='all, delete', backref='state')
-
-    def __init__(self, *args, **kwargs):
-        """ Initialization of State instance """
-        super().__init__(*args, **kwargs)
-
-    if getenv('HBNB_TYPE_STORAGE') != 'db':
-        @property
-        def cities(self):
-            """Getter Function for FileStorage"""
-            from models import storage
-            result = []
-            all_cities = storage.all(City)
-            for city in all_cities:
-                if city.state_id == self.id:
-                    result.append(city)
-            return result
+class User(BaseModel, Base):
+    """This class defines a user by various attributes"""
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship('Place', cascade='all, delete', backref='user')
+    reviews = relationship('Review', cascade='all, delete', backref='user')
